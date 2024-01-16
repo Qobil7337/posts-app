82a62eb51b6a9abc8f204ea569034f0f2e2fd217
Qobil7337 <qobil7337@mail.ru>
2024-01-16 18:40:19 +0500

    factory understood

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +17,28 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
+
+// One way to assign middleware. Another way is directly in class
+//Route::get('/dashboard', [DashboardController::class, 'index'])
+//    ->name('dashboard')
+//    ->middleware('auth');
+
+
+Route::get('/posts', [PostController::class, 'index'])
+    ->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
+
